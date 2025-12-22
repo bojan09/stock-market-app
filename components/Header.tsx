cbdfe25648ca719779fe30f3916216bf0cc6f596
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2025-12-22 08:09:36 +0100

    Header, work in progress

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 header">
+      <div className="container header-wrapper">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="Stockz logo"
+            width={140}
+            height={32}
+            className="h-8 w-auto cursor-pointer"
+          />
+        </Link>
+        <nav className="hidden sm:block">{/* NavItems */}</nav>
+        {/* User Dropdown */}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
