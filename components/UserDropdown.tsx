afe756453963f4fbc54829e436e85c6d1df8e07c
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2025-12-24 08:08:15 +0100

    UserDropdown, final commit

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useRouter } from "next/navigation";
 
+import { LogOut } from "lucide-react";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,6 +13,8 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "./ui/button";
+
+import NavItems from "./NavItems";
 
 const UserDropdown = () => {
   const router = useRouter();
@@ -28,19 +32,51 @@ const UserDropdown = () => {
           variant="ghost"
           className="flex items-center gap-3 text-gray-4 hover:bg-yellow-500"
         >
-          <Avatar>
+          <Avatar className="h-8 w-8">
             <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
+            <AvatarFallback className="bg-yellow-500 text-yellow-900 text-small font-bold">
+              {user.name[0]}
+            </AvatarFallback>
           </Avatar>
+          <div className="hidden md:flex flex-col items-start">
+            <span className="text-base font-medium text-gray-400">
+              {user.name}
+            </span>
+          </div>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      <DropdownMenuContent className="text-gray-400">
+        <DropdownMenuLabel>
+          <div className="flex relative items-center gap-3 py-2">
+            <Avatar className="h-10 w-10">
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback className="bg-yellow-500 text-yellow-900 text-small font-bold">
+                {user.name[0]}
+              </AvatarFallback>
+            </Avatar>
+
+            <div className="flex flex-col">
+              <span className="text-base font-medium text-gray-400">
+                {user.name}
+              </span>
+              <span className="text-sm text-gray-500">{user.email}</span>
+            </div>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator className="bg-gray-600" />
+        <DropdownMenuItem
+          onClick={handleSignOut}
+          className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer"
+        >
+          Log out
+          <LogOut className="h-4 w-4 mr-2 hidden sm:block" />
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator className="hidden sm:block bg-gray-600" />
+
+        <nav className="sm:hidden">
+          <NavItems />
+        </nav>
       </DropdownMenuContent>
     </DropdownMenu>
   );
