7a276b805139038a1acdafb7af1302f2ebb12d1f
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2025-12-26 08:15:07 +0100

    added new component InputField, signup - work in progress

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -2,6 +2,7 @@
 import { useForm } from "react-hook-form";
 
 import { Button } from "@/components/ui/button";
+import InputField from "@/components/forms/InputField";
 
 const SignUpPage = () => {
   const {
@@ -34,6 +35,38 @@ const SignUpPage = () => {
 
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
         {/* Inputs */}
+
+        <InputField
+          name="fullName"
+          label="Full Name"
+          placeholder="John Doe"
+          register={register}
+          error={errors.fullName}
+          validation={{ required: "Full name is required", minLength: 2 }}
+        />
+
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="contact@jsmastery.com"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: "Email name is required",
+            pattern: /^\w+@\w+\.\w+$/,
+            message: "Email address is required",
+          }}
+        />
+
+        <InputField
+          name="password"
+          label="Password"
+          placeholder="Enter a strong password"
+          type="password"
+          register={register}
+          error={errors.password}
+          validation={{ required: "Password is required", minLength: 8 }}
+        />
 
         <Button
           type="submit"
