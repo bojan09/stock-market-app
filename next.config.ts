0efe63e6230e943924d6853e6620e142c691e0fd
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2026-01-27 08:17:27 +0100

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // @ts-ignore
   eslint: {
+    // Warning: This allows production builds to successfully complete
+    // even if your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
   },
 };
 
